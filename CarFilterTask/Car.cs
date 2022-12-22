bfc496edb4ca6173d65f9a8e19b064d9eedb1af0
Huseyn Abbasov <nelbekistekan12@gmail.com>
2022-12-22 13:49:17 +0400

    1.2

---
+++ CarFilterTask/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFilterTask.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BrandId { get; set; }
+        public int TypeId { get; set; }
+        public double Price { get; set; }
+        public double Kilometers { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public bool IsNew { get; set; }
+        public virtual Brand Brand { get; set; }
+        public virtual Type Type { get; set; }
+
+    }
+}
--- CarFilterTask/CarType.cs
+++ CarFilterTask/CarType.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarFilterTask.Entities
 {
-    public class Brand
+    public class Cartype
     {
         public int Id { get; set; }
         public string Name { get; set; }
